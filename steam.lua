046719761b3ff629c071f94c3a33f9f8244e7bcf
baligins <142174077+BALIGINS@users.noreply.github.com>
2026-09-05 22:50:49 +0500

    Create steam.lua

---
+++ steam.lua
@@ -0,0 +1,39 @@
+local url = "https://raw.githubusercontent.com/BALIGINS/srptools/main/music.dfpwm"
+
+local speaker = peripheral.find("speaker")
+
+if not speaker then
+    print("Speaker не найден!")
+    return
+end
+
+local dfpwm = require("cc.audio.dfpwm")
+local decoder = dfpwm.make_decoder()
+
+local response, err = http.get(url, nil, true)
+
+if not response then
+    print("Ошибка загрузки:")
+    print(err or "Неизвестная ошибка")
+    return
+end
+
+print("Музыка загружается...")
+
+while true do
+    local chunk = response.read(16 * 1024)
+
+    if not chunk then
+        break
+    end
+
+    local audio = decoder(chunk)
+
+    while not speaker.playAudio(audio) do
+        os.pullEvent("speaker_audio_empty")
+    end
+end
+
+response.close()
+
+print("Готово!")
